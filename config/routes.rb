bd8400dbf533243ea2729b74d31363192ec71a34
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-08 14:06:06 +0200

    create route, method on pages controller, view form about pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :pages
-
+  get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/mentions_legales', to: 'pages#mentions_legales'
 end
