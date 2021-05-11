bd175fc49482d9e24c9046afa57023ee351ef518
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-05-11 17:31:38 +0200

    contact_mailer ont he way

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ApplicationMailer
+  default from: 'proxyaddress@gmail.com'
+    def send_email(items)
+        mail to:      'contact@neotravail.fr',
+             from:    items["email"],
+             reply_to: items["email"],
+             subject: items["subject"],
+             body:    items["message"]
+    end
+end
