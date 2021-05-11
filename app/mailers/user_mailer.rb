fdcff8eac237211f1028fc9535b06c85e3dc8017
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-05-11 17:15:20 +0200

    class UserMailer manque address proxy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: 'proxyaddress@gmail.com'
+    def send_email(items)
+        mail to:      'contact@neotravail.fr',
+             from:    items["email"],
+             reply_to: items["email"],
+             subject: items["subject"],
+             body:    items["message"]
+    end
 end
