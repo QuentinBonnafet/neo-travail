e36a12d259ec39d38d3f46a89ca896a4dff576a5
Simon Boutin <simboutin@gmail.com>
2021-05-20 11:06:06 +0200

    soundcloud widgets attached to episode model with seed of the 3 first episodes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @episodes = Episode.last(3).reverse
   end
 
   def contact
