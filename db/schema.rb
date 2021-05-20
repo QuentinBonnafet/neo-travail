8421b6c0c0810da1e259bd2821058b637b48b4a2
Simon Boutin <simboutin@gmail.com>
2021-05-20 11:39:56 +0200

    remove newsletter subscriber from contact model and create specific subscriber model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_074209) do
+ActiveRecord::Schema.define(version: 2021_05_20_092743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2021_05_03_074209) do
     t.string "last_name"
     t.string "email", null: false
     t.text "message"
-    t.boolean "newsletter_subscriber", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -51,6 +50,14 @@ ActiveRecord::Schema.define(version: 2021_05_03_074209) do
     t.text "description"
     t.date "publication_date"
     t.string "soundcloud_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
