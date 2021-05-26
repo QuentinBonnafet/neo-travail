021303e8d6cf952b3699ae508efd0388a59e358c
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-05-26 10:26:17 +0200

    contact controller & contact mailer on the way

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
   before_validation { self.email = email.downcase }
   validates :first_name, :last_name, :email, :message, presence: true
+  after_create :send_contact
+
+  
 end
