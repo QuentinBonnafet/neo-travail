e599a9bc58b8d13e05f41bdfca1425cd3f4e3da4
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-05-26 10:26:17 +0200

    contact controller & contact mailer on the way

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,11 @@ class ContactMailer < ApplicationMailer
   
   default from: 'quentin.bonnafet@gmail.com'
 
-    def contact(message)
-        # @name = name
-        # @email = email
-        @message = message
+    def contact()
+        @first_name = params:[first_name]
+        @last_name = params:[last_name]
+        @email = params:[email]
+        @message = params:[message]
         mail(to: 'quentin.bonnafet@gmail.com', subject: 'Contact')
     end
 
